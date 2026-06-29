059048d99d3a6f0bed11eea32e26d329a7c290ab
shubhamj <shubham.cloudblitz@gmail.com>
2026-06-29 15:34:05 +0530

    a

--- infra-pipeline.groovy
+++ infra-pipeline.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage ('PULL') {
             steps {
-
+                git branch: 'main', url: 'https://github.com/jambhulkarcloudblitz-alt/eks-infra-terraform.git'
             }
         }
         stage ('TERRAFOM-PLAN') {
